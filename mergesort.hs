0f734e7a4e0aaf068aa416f6ab119f5b4c823218
art4ul <artfulonline@gmail.com>
2016-05-19 00:54:30 +0300

    change format

--- mergesort.hs
+++ mergesort.hs
@@ -1,27 +1,27 @@
-merge :: (Ord a) => [a] -> [a] -> [a] ->[a]
-merge [] right result =  result ++ right
+module MergeSort where 
 
-merge left [] result  =  result ++ left
-
-merge left@(lhead:ltail) right@(rhead:rtail) prev 
-   | lhead < rhead = let result = prev ++ [lhead] in merge ltail right result
-   | otherwise = let result = prev ++ [rhead] in merge left rtail result
-
-
-sort :: (Ord a) => [a] -> [a] 
-sort input@(head : []) = input
-sort input@(head:tail) = 
-     let 
-        (left , right) = splitAtTheMiddle input 
-        leftSorted = sort left 
-        rightSorted = sort right
-     in 
-        merge leftSorted rightSorted []
+   merge :: (Ord a) => [a] -> [a] -> [a] ->[a]
+   merge [] right result =  result ++ right
+   merge left [] result  =  result ++ left
+   merge left@(lhead:ltail) right@(rhead:rtail) prev 
+      | lhead < rhead = let result = prev ++ [lhead] in merge ltail right result
+      | otherwise = let result = prev ++ [rhead] in merge left rtail result
 
 
-splitAtTheMiddle :: (Ord a) => [a] -> ([a],[a])
-splitAtTheMiddle x = 
-   let
-      middle = (length x) `div` 2
-   in
-     splitAt middle x 
+   sort :: (Ord a) => [a] -> [a] 
+   sort input@(head : []) = input
+   sort input@(head:tail) = 
+        let 
+           (left , right) = splitAtTheMiddle input 
+           leftSorted = sort left 
+           rightSorted = sort right
+        in 
+           merge leftSorted rightSorted []
+
+
+   splitAtTheMiddle :: (Ord a) => [a] -> ([a],[a])
+   splitAtTheMiddle x = 
+      let
+         middle = (length x) `div` 2
+      in
+        splitAt middle x 
