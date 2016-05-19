96dc58b2cdc359ceb127797b5bfd0c4c892db047
art4ul <artfulonline@gmail.com>
2016-05-20 00:33:52 +0300

    upade mergesort

--- mergesort.hs
+++ mergesort.hs
@@ -1,27 +1,20 @@
 module MergeSort where 
 
-   merge :: (Ord a) => [a] -> [a] -> [a] ->[a]
-   merge [] right result =  result ++ right
-   merge left [] result  =  result ++ left
-   merge left@(lhead:ltail) right@(rhead:rtail) prev 
-      | lhead < rhead = let result = prev ++ [lhead] in merge ltail right result
-      | otherwise = let result = prev ++ [rhead] in merge left rtail result
+sort :: (Ord a) => [a] -> [a] 
+sort input@(head : []) = input
+sort input@(head:tail) = merge leftSorted rightSorted []
+   where (left , right)     = splitAtTheMiddle input
+         leftSorted         = sort left
+         rightSorted        = sort right
+         splitAtTheMiddle x = let
+            middle = (length x) `div` 2         
+            in splitAt middle x
 
 
-   sort :: (Ord a) => [a] -> [a] 
-   sort input@(head : []) = input
-   sort input@(head:tail) = 
-        let 
-           (left , right) = splitAtTheMiddle input 
-           leftSorted = sort left 
-           rightSorted = sort right
-        in 
-           merge leftSorted rightSorted []
-
-
-   splitAtTheMiddle :: (Ord a) => [a] -> ([a],[a])
-   splitAtTheMiddle x = 
-      let
-         middle = (length x) `div` 2
-      in
-        splitAt middle x 
+merge :: (Ord a) => [a] -> [a] -> [a] ->[a]
+merge [] right result =  result ++ right
+merge left [] result  =  result ++ left
+merge left@(lhead:ltail) right@(rhead:rtail) prev 
+   | lhead < rhead    =  merge ltail right $ listWith lhead
+   | otherwise        =  merge left rtail  $ listWith rhead
+   where listWith x = prev ++ [x]
